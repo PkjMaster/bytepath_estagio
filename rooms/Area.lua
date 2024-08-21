e2d89ee4f5890205ef4abbd5b04bd543f33196d3
PkjMaster <masterpkj61@gmail.com>
2024-08-20 21:18:30 -0300

    final do cap 4

--- rooms/Area.lua
+++ rooms/Area.lua
@@ -20,6 +20,7 @@ end
 function Area:addGameObject(game_object_type, x, y, opts)
     local opts = opts or {}
     local game_object = _G[game_object_type](self, x or 0, y or 0, opts)
+    game_object.class = game_object_type
     table.insert(self.game_objects, game_object)
     return game_object
 end
@@ -27,4 +28,28 @@ end
 function Area:getGameObjects(f)
     return M.select(self.game_objects, f)
 end
+
+function Area:queryCircleArea(x, y, radius, object_types)
+    out = {}
+    for game_object in ipairs(object_types) do
+        if M.include(object_types, game_object.class) then
+            local d = distance(x, y, game_object.x, game_object.y)
+            if d <= radius then
+                out.insert(out, game_object)
+            end
+        end
+    end
+end
+
+function Area:getClosestObject(x, y, radius, object_types)
+    local objects = self:queryCircleArea(x, y, radius, object_types)
+    table.sort(objects, function(a, b)
+        local da = distance(x, y, a.x, a.y)
+        local db = distance(x, y, b.x, b.y)
+        return da < db
+    end)
+    return objects[1]
+end
+
+    
     
--- utils.lua
+++ utils.lua
@@ -40,3 +40,7 @@ function closestInt(n)
 
     print(c)
 end
+
+function distance(x1, y1, x2, y2)
+    return math.sqrt((x1 - x2)^2 + (y1 - y2)^2)
+end
